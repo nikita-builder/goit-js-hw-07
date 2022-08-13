f9da40dc62e3f362b3ca0b6c475a5023a7a5e299
nikita-builder <nikitareutov2010@yahoo.com>
2022-08-13 17:30:51 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+console.log(galleryItems);
+
+const galleryDiv = document.querySelector(".gallery");
+const galleryCard = createGalleryCards(galleryItems);
+galleryDiv.insertAdjacentHTML('afterbegin', galleryCard);
+
+function createGalleryCards(images) {
+    return images.map(image => 
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${image.original}" onclick="return false;">
+            <img
+                class="gallery__image"
+                src="${image.preview}"
+                data-source="${image.original}"
+                alt="${image.description}"
+            />
+            </a>
+            </div>
+        `).join('');
+}
+
+
+galleryDiv.addEventListener('click', openImage);
+
+function openImage(event) {
+    event.preventDefault();
+    
+    if (event.target.nodeName !== 'IMG'){
+        return;
+    };
+
+const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`,
+
+);
+
+instance.show();
+
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryDiv = document.querySelector(".gallery");
+const galleryCard = createGalleryCards(galleryItems);
+galleryDiv.insertAdjacentHTML('afterbegin', galleryCard);
+
+function createGalleryCards(images) {
+    return images.map(image => 
+`<a class="gallery__item" href="${image.original}" onclick="return false;">
+<img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+).join('');
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "alt"
+});
 
 console.log(galleryItems);
